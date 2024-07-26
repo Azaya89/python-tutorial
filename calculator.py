eebdd6affc52ae5208be5dffd0d49dc35a5e547a
Isaiah Akorita <akoritaisaiah@gmail.com>
2024-07-26 18:23:45 +0100

    upload state

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
 """
 This file contains functions for simple calculations.
 """
+def add_numbers(a,b):
+    """Returns the sum of two numbers"""
+    return a+b
--- hello.py
+++ hello.py
@@ -10,5 +10,3 @@ def get_name():
 
 if __name__ == "__main__":
     print(main())
-
-# git init, status, add, commit (-m or -am), log, log --oneline, revert, reset (--soft, --hard), rm --cached, restore
